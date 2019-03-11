117c1a04e4b356112edf9a782f77924cbff65387
luiangvia <luiangvia@gmail.com>
2019-03-11 07:10:59 -0700

    unfinished

---
+++ app/data/friends.js
@@ -0,0 +1,101 @@
+var friends = [{
+      "name": "Monkey D. Luffy",
+      "photo": "https://vignette.wikia.nocookie.net/onepiece/images/6/6d/Monkey_D._Luffy_Anime_Post_Timeskip_Infobox.png/revision/latest?cb=20190124235416",
+      "scores": [
+         "1",
+         "1",
+         "2",
+         "5",
+         "5",
+         "5",
+         "2",
+         "1",
+         "5",
+         "5"
+      ]
+   },
+   {
+      "name": "Roronoa Zoro",
+      "photo": "https://vignette.wikia.nocookie.net/onepiece/images/5/52/Roronoa_Zoro_Anime_Post_Timeskip_Infobox.png/revision/latest?cb=20141008195159",
+      "scores": [
+         "3",
+         "1",
+         "1",
+         "5",
+         "3",
+         "2",
+         "2",
+         "5",
+         "5",
+         "5"
+      ]
+   },
+   {
+      "name": "Sanji",
+      "photo": "https://vignette.wikia.nocookie.net/onepiece/images/b/b6/Sanji_Anime_Post_Timeskip_Infobox.png/revision/latest?cb=20170625125657",
+      "scores": [
+         "1",
+         "5",
+         "2",
+         "4",
+         "5",
+         "4",
+         "3",
+         "1",
+         "5",
+         "5"
+      ]
+   },
+   {
+      "name": "Nami",
+      "photo": "https://vignette.wikia.nocookie.net/onepiece/images/6/68/Nami_Anime_Post_Timeskip_Infobox.png/revision/latest?cb=20160917032958",
+      "scores": [
+         "5",
+         "3",
+         "5",
+         "2",
+         "2",
+         "3",
+         "1",
+         "4",
+         "2",
+         "3"
+      ]
+   },
+   {
+      "name": "Usopp",
+      "photo": "https://www.anime-planet.com/images/characters/usopp-77.jpg",
+      "scores": [
+         "4",
+         "3",
+         "4",
+         "1",
+         "3",
+         "3",
+         "2",
+         "5",
+         "1",
+         "4"
+      ]
+   },
+   {
+      "name": "Nico Robin",
+      "photo": "https://i.imgflip.com/1y7efi.jpg",
+      "scores": [
+         "2",
+         "3",
+         "3",
+         "3",
+         "2",
+         "3",
+         "8",
+         "1",
+         "4",
+         "5"
+      ]
+   }
+
+]
+
+//this export the array
+module.exports = friends;
